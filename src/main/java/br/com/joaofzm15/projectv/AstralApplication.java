51cbbbea784b36d00cdbf4e0bfb56d9f7e845e46
joao <joao.fzm@hotmail.com>
2022-06-08 16:53:11 -0300

    Project created

---
+++ src/main/java/br/com/joaofzm15/projectv/AstralApplication.java
@@ -0,0 +1,13 @@
+package br.com.joaofzm15.projectv;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AstralApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AstralApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/joaofzm15/projectv/AstralApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.joaofzm15.projectv;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AstralApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
