a8bcbd947f0c4b739d1590dd15c69f13473a5a95
joao <joao.fzm@hotmail.com>
2022-06-10 12:51:04 -0300

    Add improvements to board blueprint, add new resources

--- front-end/js/info.js
+++ front-end/js/info.js
@@ -3,14 +3,3 @@ function updateInfo(cardSlot) {
     document.getElementById("screen").style.backgroundImage = temporaryImage;
 }
 
-document.getElementById("playerMonsterZone5").addEventListener("mouseover", function () {
-    updateInfo(playerMonsterZone5)
-});
-
-document.getElementById("playerMonsterZone4").addEventListener("mouseover", function () {
-    updateInfo(playerMonsterZone4)
-});
-
-document.getElementById("playerMonsterZone3").addEventListener("mouseover", function () {
-    updateInfo(playerMonsterZone3)
-});
